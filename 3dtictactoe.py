fb8f8dd26b8612b8504105e70185e87f90bba9fb
Deven Mallamo <neved1234@gmail.com>
2026-04-18 09:37:07 -0400

    technically it works but is slow as dogwater

--- 3dtictactoe.py
+++ 3dtictactoe.py
@@ -1,5 +1,13 @@
+import math
+import copy
+
 class Board:
-    board = [[['.', '.', '.'] for _ in range(3)] for _ in range(3)]
+
+    def __init__(self, board = None, next_player_c = None):
+        if board == None:
+            self.board = [[['.', '.', '.'] for _ in range(3)] for _ in range(3)]
+        if next_player_c == None:
+            self.next_player_c = 'X'
 
     # Array should always be 3x3x3
     def __str__(self):  
@@ -13,59 +21,186 @@ class Board:
     
         return output
     
-    def move(self, x, y, c):
-        for layer in self.board:
-            if layer[x][y] == '.':
-                layer[x][y] = c
-                return
-        # if we make it here, this is not a legal move
-        raise Exception("This column is full")
+    def get_minimal_board_position(self):
+        return str((self.board, self.next_player_c))
     
-    def is_draw(self):
-        for layer in self.board:
-            for slice in layer:
-                if '.' in slice:
-                    return False
-        return True
-        
-    # 1 if X won, -1 if O won, 0 if not won, -2 if drawn (board filled up)
+    def move(self, move):
+        j, k = move
 
-    '''
-    Types of wins:
-    horizontal within the layer (2 directions)
-    diagonal within the layer (2 directions)
-    '''
-    def eval():
-        return
+        if self.winner() is not None:
+            #print(self)
+            raise Exception("The game has ended")
+        if move not in self.get_legal_moves():
+            #print(self)
+            raise Exception("Illegal Move: This column is full")
+        
+        for i in range(len(self.board)):
+            if self.board[i][j][k] == '.':
+                self.board[i][j][k] = self.next_player_c
+                if self.next_player_c == 'X':
+                    self.next_player_c = 'O'
+                else:
+                    self.next_player_c = 'X'
+                return
+        
+    def get_legal_moves(self):
+        if self.winner() is not None:
+            return []
+        
+        moves = []
+        for j in range(len(self.board[0])):
+            for k in range(len(self.board[0][0])):
+                if any(self.board[i][j][k] == '.' for i in range(len(self.board))):
+                    moves.append((j,k))
+
+        return moves
+            
+    def winner(self):
+        for i in range(len(self.board)):
+            result = self._is_layer_win(self.board[i])
+            if result is not None:
+                return result
+        
+        for j in range(len(self.board[0])):
+            layer = []
+            for i in range(len(self.board)):
+                layer.append(self.board[i][j])
+            result = self._is_layer_win(layer)
+            if result is not None:
+                return result
+
+        for k in range(len(self.board[0][0])):
+            layer = []
+            for i in range(len(self.board)):
+                strip = []
+                for j in range(len(self.board[0])):
+                    strip.append(self.board[i][j][k])
+                layer.append(strip)
+            result = self._is_layer_win(layer)
+            if result is not None:
+                return result
+
+        for w in ['X', 'O']:
+            if all(self.board[i][i][i] == w for i in range(len(self.board))):
+                return w
+            
+            if all(self.board[len(layer)-1-i][i][i] == w for i in range(len(self.board))):
+                return w
+            
+            if all(self.board[i][i][len(layer)-1-i] == w for i in range(len(self.board))):
+                return w
+            
+        return None
+    
+    def eval(self):
+        if self.winner() == 'X':
+            return 1
+        elif self.winner() == 'O':
+            return -1
+        else: 
+            return 0
 
     # represents a win state on a regular tic tac toe board
-    def _is_layer_win(self):
+    def _is_layer_win(self, layer):
         for w in ['X', 'O']:
-            for layer in self.board:
-                # horizontal
-                for s in layer:
-                    if all(c == w for c in s):
-                        return w
-                    
-                # vertical
-                for i in range(len(layer[0])):
-                    if all(layer[j][i] == w for j in range(len(layer))):
-                        return w
+            # k direction wins
+            for j in range(len(layer)):
+                if all(layer[j][k] == w for k in range(len(layer[j]))):
+                    return w
                 
-                # diagonal
-                if all(layer[i][i] == w for i in range(len(layer))):
+            # j direction wins
+            for k in range(len(layer[0])):
+                if all(layer[j][k] == w for j in range(len(layer))):
                     return w
+            
+            # diagonal
+            if all(layer[j][j] == w for j in range(len(layer))):
+                return w
 
-                if all(layer[i][len(layer)-1-i] == w for i in range(len(layer))):
-                    return w
-        
+            if all(layer[j][len(layer)-1-j] == w for j in range(len(layer))):
+                return w
+            
         return None
+    
+    def get_copy():
+        return
+    
 
-board = Board()
+def minimax(board, lookup = {}):
+    #print(f'Minimax Debug: getting board state as input at depth {depth}:')
+    #print(board)
+    precomputed = lookup.get(board.get_minimal_board_position())
+    if precomputed is not None:
+        return precomputed
+    
+    legal_moves = board.get_legal_moves()
+    #print(f'Minimax Debug: Found {legal_moves} legal moves for board state')
+    if len(legal_moves) == 0:
+        ##print(f'Minimax Debug: Found 0 legal moves for board state: returning evaluation {board.eval()}')
+        return board.eval(), None
+    
+    is_maximizing = board.next_player_c == 'X'
+    if is_maximizing:
+        best_score = -math.inf
+        best_move = None
+        for move in legal_moves:
+            board_copy = copy.deepcopy(board)
+            #print(f'Minimax Debug: exploring move {move} at depth {depth}, maximizing from board state {board_copy}')
+            board_copy.move(move)
+            score, _ = minimax(board_copy, lookup)
+            if score > best_score:
+                best_score = score
+                best_move = move
+                lookup[board_copy.get_minimal_board_position()] = (best_score, best_move)
+                print_progress(lookup)
+                if best_score == 1: # This indicates that we have found a move that is winning by force, we don't need to look further
+                    break 
+        return best_score, best_move
+    else:
+        best_score = math.inf
+        best_move = None
+        for move in legal_moves:
+            board_copy = copy.deepcopy(board)
+            #print(f'Minimax Debug: exploring move {move} at depth {depth} minimizing from board state {board_copy}')
+            board_copy.move(move)
+            score, _ = minimax(board_copy, lookup)
+            if score < best_score:
+                best_score = score
+                best_move = move
+                lookup[board_copy.get_minimal_board_position()] = (best_score, best_move)
+                print_progress(lookup)
+                if best_score == -1:  # This indicates that we have found a move that is winning by force, we don't need to look further
+                    break
+        return best_score, best_move
 
-board.move(0,0,'O')
-board.move(1,1,'O')
-board.move(2,0,'O')
+def print_progress(lookup):
+    num_positions = len(lookup)
+    if num_positions % 10000 == 0:
+        print("Number of positions explored:", num_positions)
 
-print(board)
-print(board._is_layer_win())
+def main():
+    board = Board()
+    eval, engine_move = minimax(board)
+    print(eval, engine_move)
+
+    # print(f"Welcome to 3D Tic Tac Toe. You are {board.next_player_c} and are going first.")
+    # while board.winner() is None and len(board.get_legal_moves()) > 0:
+    #     legal_move = False
+    #     while not legal_move:
+    #         print("Current Board State:\n\n")
+    #         print(board)
+    #         print("\n\n")
+    #         print(board.get_legal_moves())
+    #         j = int(input("X coordinate: "))
+    #         k = int(input("Y coordinate: "))
+    #         try:
+    #             board.move((j,k))
+    #             legal_move = True
+    #         except:
+    #             print("You must make a legal move")
+
+    #     eval, engine_move = minimax(board)
+    #     board.move(engine_move)
+ 
+if __name__ == "__main__":
+    main()
